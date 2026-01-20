25408e323b8ec1e7b2238f6a58d3268cc1645f15
Joandson Bezerra <36518985+joandson19@users.noreply.github.com>
2026-01-19 22:23:22 -0300

    Add files via upload

---
+++ .config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'sgp_url' => 'https://seu-sgp-url.com.br',
+    'sgp_token' => 'seu-token-aqui', 
+    'webhook_token' => 'seu-webhook-token', // Token para proteger disparo de push
+    'sgp_app_name' => 'seu-app-name',
+    'whatsapp_number' => '557500000000' // Format with country code for link,
+];
